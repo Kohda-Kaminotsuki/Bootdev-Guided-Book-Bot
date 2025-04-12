119e1bfff1af35404f266e00e4595e27b005651c
Kohda-Kaminotsuki <biggsjc@acad.sunybroome.edu>
2025-04-12 17:01:18 -0400

    Update stats.py

--- stats.py
+++ stats.py
@@ -1,3 +1,7 @@
+# turns text file to string
+def get_book_text(filepath):
+    with open(filepath) as s:
+        return s.read()
 # Prints number of words in a document
 def word_count():
     superlist = get_book_text("books/frankenstein.txt")
