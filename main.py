e631be01bd141bed223e08923072b6e9001df2a3
Kohda-Kaminotsuki <biggsjc@acad.sunybroome.edu>
2025-04-12 16:55:36 -0400

    0.7 added stats py to refactor, nano sucks, gonna do through git

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 def get_book_text(filepath):
     with open(filepath) as s:
         return s.read()
-
+# Prints the entire text of the document
 def main():
     print(get_book_text("books/frankenstein.txt"))
 
-main()
+# Prints number of words in a document
+def word_count():
+    superlist = get_book_text("books/frankenstein.txt")
+    return len(superlist.split())
+
+total_words = word_count()
+print(f"{total_words} words found in the document")
