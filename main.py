ecca29928dd8a555d423e7c726e5f2b3f76b24c7
Kohda-Kaminotsuki <biggsjc@acad.sunybroome.edu>
2025-04-12 19:00:46 -0400

    0.10 Inputs process right

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
+import sys
+input = sys.argv
+if len(input) != 2:
+    print(f"Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+else:
+    path_to_book = sys.argv[1]
 from stats import word_count
 from stats import character_count
 # Prints the entire text of the document
 def main():
-    print(get_book_text("books/frankenstein.txt"))
+    print(get_book_text(path_to_book))
 
 
 # Debug Check
-total_words = word_count()
+total_words = word_count(path_to_book)
 #print(word_count())
 #print(character_count())
-character_count_list = character_count()
+character_count_list = character_count(path_to_book)
 sorted_character_count = (dict(sorted(character_count_list.items(), key=lambda item: item[1], reverse=True)))
 
 
 
 
 print("============ BOOKBOT ============")
-print("Analyzing book found at books/frankenstein.txt...")
+print(f"Analyzing book found at {path_to_book}...")
 print("----------- Word Count ----------")
 print(f"Found {total_words} total words")
 print("--------- Character Count -------")
--- stats.py
+++ stats.py
@@ -1,13 +1,13 @@
 # turns text file to string
-def get_book_text(filepath):
-    with open(filepath) as s:
+def get_book_text(path):
+    with open(path) as s:
         return s.read()
 # Prints number of words in a document
-def word_count():
-    superlist = get_book_text("books/frankenstein.txt")
+def word_count(path):
+    superlist = get_book_text(path)
     return len(superlist.split())
-def character_count():
-    superlist = get_book_text("books/frankenstein.txt")
+def character_count(path):
+    superlist = get_book_text(path)
     output_dictionary = {"a":0}
     for c in superlist:
         character = c.lower()
