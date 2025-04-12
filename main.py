eb92dadad4b0d21332c0bae7ad6510a5d71a17c2
Kohda-Kaminotsuki <biggsjc@acad.sunybroome.edu>
2025-04-12 17:00:42 -0400

    Update main.py
    Moving superlist to stats

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-def get_book_text(filepath):
-    with open(filepath) as s:
-        return s.read()
 # Prints the entire text of the document
 def main():
     print(get_book_text("books/frankenstein.txt"))
