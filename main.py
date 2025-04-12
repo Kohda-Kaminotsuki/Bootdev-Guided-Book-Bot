a2b6d145086c243928936f002b197f71e75a5927
Kohda-Kaminotsuki <biggsjc@acad.sunybroome.edu>
2025-04-12 16:40:02 -0400

    0.6 Setup main.py to print the entire book file of frankenstein

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath) as s:
+        return s.read()
+
+def main():
+    print(get_book_text("books/frankenstein.txt"))
+
+main()
